325997ff0ecbdce12764d2ffd71bd7613a732c2f
Waydon <waydon.saal@younglings.africa>
2023-06-21 09:40:25 +0200

    #

---
+++ my-project/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function App() {
+  return (
+    <nav>
+    <h1>Bob's Bistro</h1>
+    <ul>
+        <li>Menu</li>
+        <li>About</li>
+        <li>Contact</li>
+    </ul>
+  </nav>
+  )
+}
+
+export default App;
